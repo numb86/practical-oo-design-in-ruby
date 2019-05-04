39444948c62b72b315c2ebcf85047bea0664b93f
numb86 <86numb@gmail.com>
2019-05-04 12:12:25 +0900

    142 6_2_1.rb

---
+++ chapter6/6_2_1.rb
@@ -0,0 +1,21 @@
+class Bicycle
+  attr_reader :size, :tape_color
+
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  def spares
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
+  end
+end
+
+bike = Bicycle.new(size: 'M', tape_color: 'red')
+
+p bike.size # "M"
+p bike.spares # {:chain=>"10-speed", :tire_size=>"23", :tape_color=>"red"}
